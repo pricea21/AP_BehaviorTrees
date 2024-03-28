b201b7aac3494667b66e9fce1d0a3bfc4a62fb3d
pricea21 <pricea26@my.erau.edu>
2024-03-27 20:50:51 -0700

    assets

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public bool isClosed = false;
+    public bool isLocked = false;
+
+    Vector3 closedRotation = new Vector3(0, 0, 0);
+    Vector3 openRotation = new Vector3(0, -135, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isClosed)
+        {
+            transform.eulerAngles = closedRotation;
+        }
+        else
+        {
+            transform.eulerAngles = openRotation;
+        }
+    }
+
+    public void OpenClose(bool isOpen)
+    {
+        if (isOpen)
+        {
+            Open();
+        }
+        else
+        {
+            Close();
+        }
+        return;
+    }
+
+    public void UnlockLock(bool Lock)
+    {
+        isLocked = Lock;
+    }
+    public bool Open()
+    {
+        if (isClosed && !isLocked)
+        {
+            Debug.Log("door is now open");
+            isClosed = false;
+            transform.eulerAngles = openRotation;
+            return true;
+        }
+
+        Debug.Log("door was either locked or already open");
+        return false;
+    }
+
+    public bool Close()
+    {
+        if (!isClosed)
+        {
+            Debug.Log("door is now closed");
+            transform.eulerAngles = closedRotation;
+            isClosed = true;
+        }
+        return true;
+    }
+}
